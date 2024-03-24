4a3b8e535aa53d355f5300a89c76b744ad2a0091
David Huculak <davidhuculak5@gmail.com>
2024-03-24 14:56:07 -0400

    set request mode to use an enum value

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,12 @@ use wasm_bindgen::prelude::*;
 
 use web_sys::{Request, RequestInit};
 
-// Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
     let val = document.create_element("p")?;
     val.set_inner_html("Hello from Rust!");
 
@@ -19,6 +15,7 @@ fn main() -> Result<(), JsValue> {
 
     let mut opts = RequestInit::new();
     opts.method("GET");
+    opts.mode(web_sys::RequestMode::SameOrigin);
     let request = Request::new_with_str_and_init("resource.png", &opts)?;
 
     let _response_promise = window.fetch_with_request(&request);
